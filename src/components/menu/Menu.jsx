bef6fd1b8b36afe3bdf4f033a1470ae20c2c08cb
unknown <xeyyamomarov2014@gmail.com>
2022-10-20 17:17:45 +0400

    /

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ export const Menu = ({ menuOpen, setMenuOpen }) => {
           <a href="#intro">Home</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#portfolio">Portfolio</a>
+          <a href="#portfolio">Projects</a>
         </li>
         {/* <li onClick={() => setMenuOpen(false)}>
           <a href="#works">Works</a>
